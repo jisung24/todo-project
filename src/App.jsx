dda607d749358dd31af1cd1c73776c780121fdfc
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-08-18 15:04:55 +0900

    [✨Refactor]이미 선언된 함수가 무차별적으로 계속 다른 주소값으로 선언되는 것 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useEffect, useCallback } from 'react'
 import TodoLogo from './components/header/TodoLogo'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
@@ -20,13 +20,13 @@ function App() {
     setInputValue((prev) => e.target.value)
   }
 
-  const checkLength = (inputLength) => {
+  const checkLength = useCallback((inputLength) => {
     return inputLength >= MIN_LENGTH && inputLength <= MAX_LENGTH
-  }
+  }, [])
 
-  const checkDuplicate = (findValue) => {
+  const checkDuplicate = useCallback((findValue) => {
     return todos.find((e) => e.content === findValue)
-  }
+  }, [])
   const handleInputSubmit = (e) => {
     if (checkLength(inputValue.length) && !checkDuplicate(inputValue)) {
       e.preventDefault()
@@ -44,26 +44,60 @@ function App() {
     }
   }
 
-  const toggleTodos = (id) => {
-    const findTodoElement = todos.find((e) => e.id === id)
-    findTodoElement.completed = !findTodoElement.completed
-    setTodos((prev) => [...prev]) // []를 새로 씌워줘야, 배열 주소값이 바껴 값이 바뀌고, set이 호출된다
-  }
+  // 여기있는 함수들은 전부 재랜더링 되면 계속 재선언이 돼...!
+  const toggleTodos = useCallback(
+    (id) => {
+      // todos를 의존성 배열로 추가를 안 하면,
+      // 계속 저 dummy Data로 toggle을 해야 돼!
+      // todos를 추가했으면, 그 추가된 todos도 toggle이 돼야 해..!
+      const findTodoElement = todos.find((e) => e.id === id)
+      findTodoElement.completed = !findTodoElement.completed
+      setTodos((prev) => [...prev]) // []를 새로 씌워줘야, 배열 주소값이 바껴 값이 바뀌고, set이 호출된다
+    },
+    [todos],
+  )
 
-  const deleteTodos = (id) => {
-    const deletedElement = todos.filter((e) => e.id !== id)
-    setTodos((prev) => [...deletedElement])
-  }
+  const deleteTodos = useCallback(
+    (id) => {
+      const deletedElement = todos.filter((e) => e.id !== id)
+      setTodos((prev) => [...deletedElement])
+    },
+    [todos],
+  )
 
   // 그냥 함수겉에 감싸는거야
   // 그냥 그 함수를 선언할 때 바로 감싸주면 됨
   // 즉, 값과 관련된 함수 => 매번 호출될 필요가 없음
   // 그 값과 관련된 변수가 변하는게 아니라면 호출 필요없어
-  const allTodos = useMemo(() => todos.length)
-  const completedTodos = useMemo(() => todos.filter((e) => e.completed).length)
+  const allTodos = useMemo(() => todos.length, [todos])
+  const completedTodos = useMemo(
+    () => todos.filter((e) => e.completed).length,
+    [todos],
+    // 만약 여기 함수를 넣어줬으면... 안 되는게
+    // 함수에 useCallback이 없지않은이상 계속 재선언 돼서
+    // 메모리가 계속 바껴...
+    // 그래서 재랜더링 될 때 마다 아마 completedTodos가 호출이 될거야...!
+  )
   const unCompletedTodos = useMemo(() => {
     return todos.filter((e) => !e.completed).length
-  })
+  }, [todos])
+
+  // 이 함수들이 전부 재랜더링 될 때 마다 계속 호출됨.
+  // 함수도 객체라서, 함수이름에 계속 다른 주소값이 들어감.
+  // 어차피 1번만 선언된 후에 계속 호출해서 쓰면 됨.
+  // 다시 선언이 되어야 할 이유가 있나...?
+  useEffect(() => {
+    console.log('toggleTodos')
+  }, [toggleTodos])
+  useEffect(() => {
+    console.log('deleteTodos')
+  }, [deleteTodos])
+  useEffect(() => {
+    console.log('checkLength')
+  }, [checkLength])
+  useEffect(() => {
+    console.log('checkDuplicate')
+  }, [checkDuplicate])
   return (
     <div className="App">
       <header>
--- src/components/header/TodoLogo.jsx
+++ src/components/header/TodoLogo.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+// React.memo : 파라미터가 일반 변수면 값만 안 달라지면 괜찮음
+// 하지만, 함수나, 객체일경우...! => 부모에서 useCallback을 사용하지 않아, 함수가 재선언이 되면,
+// 다른 메모리를 가리켜, 파라미터가 바뀐것으로 취급, => 재랜더링 됨.
 
+// 함수 전달할 때 조심하자 => 굳이 계속 랜더링해야할게 아니라면, useCallback
 export default React.memo(function TodoLogo({ logoText }) {
   return (
     <h2>
